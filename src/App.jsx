26d0b7511dcd5e57d852239cf1360543d61667d2
mataraimov <nuradil.mataraimov@alatoo.edu.kg>
2024-02-13 21:22:18 +0600

    router

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        {/* <Route path='/' element={} /> */}
+        {/* <Route path='/' element={} /> */}
+      </Routes>
+    </>
+  );
+}
+
+export default App;
