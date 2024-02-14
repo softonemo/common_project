a7ca62978dcec4b781b199b82787e588e0cc5bf2
mataraimov <nuradil.mataraimov@alatoo.edu.kg>
2024-02-14 20:47:30 +0600

    details

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Detail from './details/Detail';
+import DetailPage from './pages/DetailPage';
 
 function App() {
   return (
     <>
       <Routes>
-        {/* <Route path='/' element={} /> */}
-        {/* <Route path='/' element={} /> */}
+        <Route path="/flowers/:id" element={<DetailPage />} />
+        <Detail />
       </Routes>
     </>
   );
---
+++ src/pages/DetailPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Detail from '../details/Detail';
+
+function DetailPage() {
+  return (
+    <div>
+      DetailPage
+      <Detail />
+    </div>
+  );
+}
+
+export default DetailPage;
