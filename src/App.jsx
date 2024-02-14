6178274a1539227fe8e0935e3683ba5002f307a1
softonemo <narimannarynbekov@gmail.com>
2024-02-14 20:46:33 +0600

    add mainPage v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import MainPage from './components/mainpage';
 
 function App() {
   return (
     <>
       <Routes>
-        {/* <Route path='/' element={} /> */}
+        <Route path='/' element={<MainPage/>} />
         {/* <Route path='/' element={} /> */}
       </Routes>
     </>
