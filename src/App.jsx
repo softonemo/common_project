b350061802d8cb58daca5af06828214692fb6a7c
Muktarbekova312 <kk_muktarbek@icloud.com>
2024-02-15 18:47:20 +0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header';
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <>
+      <Header/>
       <Routes>
         {/* <Route path='/' element={} /> */}
-        {/* <Route path='/' element={} /> */}
+        <Route path='/Login' element={<Login/>} />
       </Routes>
     </>
   );
